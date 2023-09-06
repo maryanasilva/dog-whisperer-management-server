34db44765414c549a1b1d855881683f10c786214
Mariana Silva <maryana_silva@sapo.pt>
2023-09-06 10:18:20 +0100

    kennels route

--- routes/dogs.routes.js
+++ routes/dogs.routes.js
@@ -9,11 +9,37 @@ const User = require("../models/User.model");
 
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
-// POST route that creates a new Dog
-router.post("/dogs", async (req, res) => {
-  const { name, age, description, genre, size, image } = req.body;
+// HTTP Verbs: GET, POST, PUT, DELETE
+// Since we're building a REST API, we're sending data via JSON, and using HTTP Requests for communication.
+
+// GET route that gets all the dogs
+router.get("/dogs", async (req, res) => {
   try {
-    let response = await Kennel.create({
+    let allDogs = await Dog.find().populate("kennel");
+    res.json(allDogs);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// GET route that gets a specific dog
+router.get("/dogs/:kennelId", async (req, res) => {
+  const { kennelId } = req.params;
+  try {
+    let foundDog = await Kennel.findById(kennelId).populate("dogs");
+    res.json(foundDog);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// POST route to create a dog
+router.post("/:kennelId/kennels", isAuthenticated, async (req, res) => {
+  const { kennelId } = req.params;
+  const { name, age, description, genre, size, image } = req.body;
+  const user = req.payload;
+  try {
+    let newDog = await Dog.create({
       name,
       age,
       description,
@@ -21,17 +47,28 @@ router.post("/dogs", async (req, res) => {
       size,
       image,
     });
-    res.json(response);
-  } catch (error) {
-    res.json(error);
-  }
-});
 
-// GET route that gets all the dogs
-router.get("/dogs", async (req, res) => {
-  try {
-    let allDogs = await Dog.find().populate("kennel");
-    res.json(allDogs);
+    // new dog id -> kennel
+    await Kennel.findByIdAndUpdate(kennelId, {
+      $push: { dogs: newDog._id },
+    });
+
+    // kennel id -> dog
+    await Dog.findByIdAndUpdate(newDog._id, {
+      $push: { kennel: kennelId },
+    });
+
+    // user id -> dog
+    await Dog.findByIdAndUpdate(newDog._id, {
+      $push: { owner: user._id },
+    });
+
+    // owned dog through new dog id -> user
+    await User.findByIdAndUpdate(user._id, {
+      $push: { ownedDogs: newDog._id },
+    });
+
+    res.json(newDog);
   } catch (error) {
     res.json(error);
   }
@@ -48,20 +85,7 @@ router.get("/singledog/:dogId", async (req, res) => {
   }
 });
 
-// GET route that gets the info of a specific dog
-router.get("/dogs/:kennelId", async (req, res) => {
-  const { kennelId } = req.params;
-  try {
-    let foundDog = await Kennel.findById(kennelId).populate("dogs");
-    res.json(foundDog);
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-// HTTP Verbs: GET, POST, PUT, DELETE
-// Since we're building a REST API, we're sending data via JSON, and using HTTP Requests for communication.
-// PUT routes to update info of a kennel
+// PUT route to update info of a dog
 router.put("/dogs/:dogId", async (req, res) => {
   const { dogId } = req.params;
   const { name, age, description, genre, size, image } = req.body;
@@ -83,46 +107,6 @@ router.delete("/dogs/:dogId", async (req, res) => {
   try {
     await Dog.findByIdAndDelete(dogId);
     res.json({ message: "Dog deleted" });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-// CREATE a route to create a dog
-router.post("/:kennelId/kennels", isAuthenticated, async (req, res) => {
-  const { kennelId } = req.params;
-  const { name, age, description, genre, size, image } = req.body;
-  const user = req.payload;
-  try {
-    let newDog = await Dog.create({
-      name,
-      age,
-      description,
-      genre,
-      size,
-      image,
-    });
-
-    // new dog id -> kennel
-    await Kennel.findByIdAndUpdate(kennelId, {
-      $push: { dogs: newDog._id },
-    });
-
-    // kennel -> dog
-    await Dog.findByIdAndUpdate(newDog._id, {
-      $push: { kennel: kennelId },
-    });
-
-    // user -> dog
-    await Dog.findByIdAndUpdate(newDog._id, {
-      $push: { owner: user._id },
-    });
-
-    await User.findByIdAndUpdate(user._id, {
-      $push: { ownedDogs: newDog._id },
-    });
-
-    res.json(newDog);
   } catch (error) {
     res.json(error);
   }
--- routes/kennel.routes.js
+++ routes/kennel.routes.js
@@ -1,88 +1,96 @@
 // Require Packages / Packages Funcionalities
-const router = require('express').Router();
-const mongoose = require('mongoose');
+const router = require("express").Router();
+const mongoose = require("mongoose");
 
 // Require Data Models
-const Kennel = require('../models/Kennel.model');
-const Dog = require('../models/Dog.model');
-
-// POST route that creates a new kennel
-router.post('/kennels', async (req,res) => {
-    const {name, description, location, image} = req.body;
-    try {
-        let response = await Kennel.create({name, description, location, image, dogs:[]});
-        res.json(response);
-    }
-    catch (error) {
-        res.json(error);
-    }
-});
-
-// GET route that gets all the kennels
-router.get('/kennels', async (req,res) => {
-    try {
-        let allKennels = await Kennel.find().populate('dogs');
-        res.json(allKennels);
-    }
-    catch (error) {
-        res.json(error);
-    }
-});
-
-// GET route that gets the info of a specific project
-router.get('/kennels/:kennelId', async (req,res) => {
-    const {kennelId} = req.params;
-    try {
-        let foundKennel = await Kennel.findById(kennelId).populate('dogs');
-        res.json(foundKennel)
-    }
-    catch (error) {
-        res.json(error)
-    }
-});
+const Kennel = require("../models/Kennel.model");
+const Dog = require("../models/Dog.model");
 
 // HTTP Verbs: GET, POST, PUT, DELETE
 // Since we're building a REST API, we're sending data via JSON, and using HTTP Requests for communication.
-// PUT routes to update info of a kennel
-router.put('/kennels/:kennelId', async (req,res) => {
-    const {kennelId} = req.params;
-    const {name, description, location, image}= req.body;
-    try {
-        let updateKennel = await Kennel.findByIdAndUpdate(kennelId, {name, description, location, image}, {new: true});
-        res.json(updateKennel);
-    }
-    catch (error) {
-        res.json(error);
-    }
+
+// GET route that gets all the kennels
+router.get("/kennels", async (req, res) => {
+  try {
+    let allKennels = await Kennel.find().populate("dogs");
+    res.json(allKennels);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
-// DELETE route to delete a kennel
-router.delete('/kennels/:kennelId', async (req,res) => {
-    const {kennelId} = req.params;
-    try {
-        await Kennel.findByIdAndDelete(kennelId);
-        res.json({message: 'Kennel deleted'});
-    }
-    catch (error) {
-        res.json(error);
-    }
+// GET route that gets a specific kennels
+router.get("/kennels/:kennelId", async (req, res) => {
+  const { kennelId } = req.params;
+  try {
+    let foundKennel = await Kennel.findById(kennelId).populate("dogs");
+    res.json(foundKennel);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
-router.post('/:kennelId/dogs', async (req,res) => {
-    const {kennelId} = req.params;
-    const {name, description, location, image} = req.body;
-    try {
-        let newDog = await Dog.create({name, description, location, image, kennel: kennelId});
+// POST route to create a kennels
+router.post("/:kennelId/dogs", async (req, res) => {
+  const { kennelId } = req.params;
+  const { name, description, location, image } = req.body;
+  try {
+    let newDog = await Kennel.create({
+      name,
+      description,
+      location,
+      image,
+      kennel: kennelId,
+      dogs: [],
+    });
 
-        let response = await Kennel.findByIdAndUpdate(kennelId, {$push: {dogs: newDog._id}});
+    let response = await Kennel.findByIdAndUpdate(kennelId, {
+      $push: { dogs: newDog._id },
+    });
 
-        res.json(response);
-    }
-    catch (error) {
-        res.json(error);
-    }
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
+// GET route that gets the info of a specific kennels to update
+router.get("/singlekennels/:kennelId", async (req, res) => {
+  const { kennelId } = req.params;
+  try {
+    let foundKennel = await Dog.findById(kennelId).populate("dog");
+    res.json(foundKennel);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// PUT route to update info of a kennels
+router.put("/kennels/:kennelId", async (req, res) => {
+  const { kennelId } = req.params;
+  const { name, description, location, image } = req.body;
+  try {
+    let updateKennel = await Kennel.findByIdAndUpdate(
+      kennelId,
+      { name, description, location, image },
+      { new: true }
+    );
+    res.json(updateKennel);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// DELETE route to delete a kennels
+router.delete("/kennels/:kennelId", async (req, res) => {
+  const { kennelId } = req.params;
+  try {
+    await Kennel.findByIdAndDelete(kennelId);
+    res.json({ message: "Kennel deleted" });
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 // Exporting Express Router with all its routes
 module.exports = router;
